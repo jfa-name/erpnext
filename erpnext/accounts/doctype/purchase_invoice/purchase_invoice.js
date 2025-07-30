a94fa5019042ccaac78c263502eb166c497f52ea
mithili <mithili15602@gamil.com>
2025-07-30 11:19:23 +0530

    fix: add condition to fetch active accounts
    (cherry picked from commit 7c8dd86a35ee3029404943542358ec66430edec1)

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice.js
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice.js
@@ -31,7 +31,6 @@ erpnext.accounts.PurchaseInvoice = class PurchaseInvoice extends erpnext.buying.
 				query: "erpnext.controllers.queries.get_expense_account",
 				filters: {
 					company: doc.company,
-					disabled: 0,
 				},
 			};
 		});
